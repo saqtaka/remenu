866cb16e9d24ffc013d6fa93e4168d7b8c3dabd5
make-ship <50040766+make-ship@users.noreply.github.com>
2021-03-20 15:33:38 +0900

    setting

---
+++ components/component/LocaleSetting.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <div class="locale">
+        <v-select
+          v-model="locale"
+          :items="items"
+          @change="selectChange"
+        />
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+import getLocale from '@/javascript/GetLocale'
+
+export default {
+  data () {
+    return {
+      locale: '',
+      items: [{
+        text: 'English',
+        value: 'en-US'
+      },
+      {
+        text: '日本語',
+        value: 'ja-JP'
+      }]
+    }
+  },
+  mounted () {
+    // console.log('get1')
+    const self = this
+    firebase.auth().onAuthStateChanged((user) => {
+      getLocale(firebase.auth().currentUser.uid)
+        .then(function (value) {
+          self.locale = value
+        })
+        .catch(function (error) {
+          console.error(error)
+        })
+    })
+  },
+  methods: {
+    selectChange () {
+      this.$i18n.locale = this.locale
+
+      firebase
+        .firestore()
+        .collection('users')
+        .doc(firebase.auth().currentUser.uid)
+        .set({
+          uid: firebase.auth().currentUser.uid,
+          locale: this.locale
+        }, { merge: true })
+        .then(function (docRef) {
+          console.log('success')
+        })
+        .catch(function (error) {
+          console.error(error.message)
+        })
+    }
+  }
+}
+</script>
---
+++ pages/Setting.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-container
+      class="fill-height"
+      fluid
+    >
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col
+          cols="12"
+          sm="6"
+          class="text-center"
+        >
+          <LocaleSetting />
+          <!-- <SelectLocale></SelectLocale> -->
+        </v-col>
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <Helper />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+// import SelectLocale from '@/components/basic/SelectLocale.vue'
+
+import Helper from '@/components/component/Helper'
+import LocaleSetting from '@/components/component/LocaleSetting'
+
+export default {
+  name: 'SettingPage',
+  components: {
+    // SelectLocale
+    Helper,
+    LocaleSetting
+  }
+}
+</script>

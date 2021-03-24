be1e6036f689e858fb9612175f37aaa004bdf332
make-ship <50040766+make-ship@users.noreply.github.com>
2021-03-24 22:28:21 +0900

    Create createanoanonymousaccount.vue

---
+++ pages/createanoanonymousaccount.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>loadnig...</p>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+export default {
+  created () {
+    firebase.auth().signInAnonymously()
+      .then(() => {
+        // Signed in..
+        this.$router.push('/habit')
+      })
+      .catch((error) => {
+        // const errorCode = error.code
+        // const errorMessage = error.message
+        console.error(error.message)
+        // ...
+      })
+  }
+}
+</script>

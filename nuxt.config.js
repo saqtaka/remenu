a20cbc8f04038b1067ecf5a876174262bf5c778d
make-ship <50040766+make-ship@users.noreply.github.com>
2021-03-25 14:28:18 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Remenuは習慣を管理できるTODOアプリです。' },
+      { hid: 'og:description', name: 'og:description', content: 'Remenuは習慣を管理できるTODOアプリです。' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }

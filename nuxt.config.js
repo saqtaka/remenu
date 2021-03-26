e8d7e84398ab9579b4fe2b77ffd1fb3232fdd2ce
make-ship <50040766+make-ship@users.noreply.github.com>
2021-03-26 15:53:47 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,16 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      {
+        type: 'text/javascript',
+        innerHTML: `(function(c,l,a,r,i,t,y){
+                      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "5zf851uwlt");`
+      }
     ]
   },
 

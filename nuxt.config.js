bcf027a10463c5d5fd411f52846aebc202a92f16
make-ship <50040766+make-ship@users.noreply.github.com>
2021-03-26 12:23:25 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-import colors from 'vuetify/es5/util/colors'
+// import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
@@ -48,7 +48,7 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: 'ja'
     }
   },
 
@@ -240,14 +240,45 @@ export default {
     theme: {
       // dark: true,
       themes: {
+        light: {
+          primary: '#536dfe',
+          primaryConst: '#536dfe',
+          secondary: '#FF5C93',
+          secondaryConst: '#FF5C93',
+          error: '#6e6e6e',
+          warning: '#FFC260',
+          success: '#3CD4A0',
+          successConst: '#3CD4A0',
+          info: '#9013FE',
+          greyDefault: '#e0e0e0',
+          greyBold: '#4a4a4a',
+          greyBoldConst: '#4a4a4a',
+          greyMedium: '#6e6e6e',
+          greyTint: '#b9b9b9',
+          default: '#e0e0e0',
+          iconColor: '#FFFFFF59'
+        },
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          // primary: colors.blue.darken2,
+          // accent: colors.grey.darken3,
+          // secondary: colors.amber.darken3,
+          // info: colors.teal.lighten1,
+          // warning: colors.amber.base,
+          // error: colors.deepOrange.accent4,
+          // success: colors.green.accent3
+          primary: '#536dfe',
+          primaryConst: '#536dfe',
+          secondary: '#FF5C93',
+          secondaryConst: '#FF5C93',
+          success: '#3CD4A0',
+          successConst: '#3CD4A0',
+          error: '#bfbfbf',
+          greyDefault: '#23232d',
+          greyMedium: '#bfbfbf',
+          greyBold: '#d6d6d6',
+          greyBoldConst: '#4a4a4a',
+          greyTint: '#9b9b9b',
+          default: '#e0e0e0'
         }
       }
     },

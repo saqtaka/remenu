3951eb4289c8716171179ac6f80032123b9e745a
make-ship <50040766+make-ship@users.noreply.github.com>
2021-04-04 21:52:35 +0900

    習慣化率を表示

---
+++ components/basic/habit/AchivementRate.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    {{ achivementRate }}%
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+export default {
+  props: {
+    habitIdProp: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      achivementRate: 0
+    }
+  },
+  // watch: {
+  //   habitIdProp: {
+  //     handler () {
+  //       const self = this
+  //       firebase
+  //         .firestore()
+  //         .collection('habitLatestLists')
+  //         .doc(this.habitIdProp)
+  //         .get()
+  //         .then(function (doc) {
+  //           self.achivementRate = doc.data().achivementRate
+  //         })
+  //     }
+  //   }
+  // },
+  created () {
+    console.log(this.habitIdProp)
+    const self = this
+    firebase
+      .firestore()
+      .collection('habitLatestLists')
+      .doc(this.habitIdProp)
+      .get()
+      .then(function (doc) {
+        self.achivementRate = doc.data().achivementRate
+      })
+  }
+}
+</script>
--- components/component/HabitCard.vue
+++ components/component/HabitCard.vue
@@ -4,16 +4,38 @@
     rounded="xl"
     @click="clearButton"
   >
-    <v-card-title>
+    <!-- <v-card-title>
       <div class="body-1">
         {{ habitData.title }}
       </div>
+    </v-card-title> -->
+
+    <v-card-title class="pa-1">
+      <v-container class="pa-0 ma-0">
+        <v-row align="center" class="pa-0 ma-0">
+          <v-col justify="center" cols="2" sm="1" class="pa-0 ma-0">
+            <div>
+              <AchivementRate :habit-id-prop="habitData.id" />
+            </div>
+          </v-col>
+          <v-col class="pa-0 ma-0">
+            <div class="ma-1 text-left">
+              <span class="body-1">{{ habitData.title }}</span>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-card-title>
   </v-card>
 </template>
 
 <script>
+import AchivementRate from '@/components/basic/habit/AchivementRate.vue'
+
 export default {
+  components: {
+    AchivementRate
+  },
   // props: ['habitData'],
   props: {
     habitData: {

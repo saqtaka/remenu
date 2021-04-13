fdebe06443ca30a5ec68b6e5fb54e14e9a40f340
make-ship <50040766+make-ship@users.noreply.github.com>
2021-04-13 10:20:37 +0900

    習慣実施カレンダー

---
+++ components/component/HabitCalendar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-date-picker
+      v-model="dates"
+      multiple
+      readonly
+    />
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import dayjs from 'dayjs'
+
+export default {
+  props: {
+    habitIdProp: {
+      type: String,
+      default: ''
+    }
+  },
+  data: () => ({
+    dates: [],
+    habitId: ''
+  }),
+  watch: {
+    habitIdProp: {
+      handler (newValue, oldValue) {
+        this.getDates(newValue)
+      }
+    }
+  },
+  methods: {
+    getDates () {
+      const self = this
+      firebase
+        .firestore()
+        .collection('habitLatestLists')
+        .doc(this.habitIdProp)
+        .onSnapshot((doc) => {
+          self.dates = doc.data().latestList.map(function (value) {
+            // console.log(dayjs(new Date(value.toDate())).format('YYYY-MM-DD'))
+            return dayjs(new Date(value.toDate())).format('YYYY-MM-DD')
+          })
+        })
+    }
+  }
+}
+</script>
--- pages/habit/_habitid.vue
+++ pages/habit/_habitid.vue
@@ -10,12 +10,31 @@
       >
         <v-col
           class="text-center"
+          cols="12"
+          sm="6"
         >
           <v-card rounded="xl">
             <v-card-text>
               <HabitEditForm :send-habit="inputHabit" />
             </v-card-text>
           </v-card>
+        </v-col>
+        <v-col
+          class="text-center"
+          cols="12"
+          sm="6"
+        >
+          <v-card rounded="xl">
+            <v-card-text>
+              <HabitCalendar :habit-id-prop="inputHabit" />
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          class="text-center"
+        >
           <div class="my-4">
             <HabitDelete :get-habit="inputHabit" />
           </div>
@@ -28,6 +47,7 @@
 <script>
 import HabitEditForm from '@/components/component/HabitEditForm.vue'
 import HabitDelete from '@/components/component/HabitDelete.vue'
+import HabitCalendar from '@/components/component/HabitCalendar.vue'
 
 export default {
   name: 'HabitEditorPage',
@@ -39,7 +59,8 @@ export default {
   // },
   components: {
     HabitEditForm,
-    HabitDelete
+    HabitDelete,
+    HabitCalendar
   },
   props: {
     habitId: {

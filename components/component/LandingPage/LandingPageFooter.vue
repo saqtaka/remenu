fdbc3381dc8fa708adf48b9aa01dfec1080e3fd3
saqtaka <50040766+saqtaka@users.noreply.github.com>
2021-04-23 13:28:18 +0900

    Revert "一度コミット"
    This reverts commit 967c383a1fb88ebd0ad9632ce93d90576b9ea3e3.

---
+++ components/component/LandingPage/LandingPageFooter.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <v-footer color="transparent">
+      <v-container class="py-5">
+        <v-row>
+          <v-col cols="12" md="4">
+            <div class="text-h6 font-weight-bold text-uppercase mb-5">
+              Account
+            </div>
+            <div class="d-flex mb-2">
+              <v-icon color="secondary lighten-1" class="mr-2">
+                {{ mdiTwitterSvgPath }}
+              </v-icon>
+              <a href="https://twitter.com/saqtaka" class="text-decoration-none secondary--text text--lighten-2">@saqtaka</a>
+            </div>
+            <div class="d-flex mb-2">
+              <v-icon color="secondary lighten-1" class="mr-2">
+                {{ mdiGithubSvgPath }}
+              </v-icon>
+              <a href="https://github.com/saqtaka" class="text-decoration-none secondary--text text--lighten-2">@saqtaka</a>
+            </div>
+          </v-col>
+          <v-col cols="12" md="4">
+            <div class="text-h6 font-weight-bold text-uppercase mb-5">
+              blog
+            </div>
+            <!-- <div class="d-flex mb-2">
+              <v-icon color="secondary lighten-1" class="mr-2">
+                {{ mdiPostSvgPath }}
+              </v-icon>
+              <a href="https://saqtaka.com" class="text-decoration-none secondary--text text--lighten-2">saqtaka.com</a>
+            </div> -->
+            <!-- <div class="d-flex mb-2">
+              <v-icon color="secondary lighten-1" class="mr-2">
+                {{ mdiPostSvgPath }}
+              </v-icon>
+              <a href="https://portability.info" class="text-decoration-none secondary--text text--lighten-2">portability.info</a>
+            </div> -->
+            <div class="d-flex mb-2">
+              <v-icon color="secondary lighten-1" class="mr-2">
+                {{ mdiPostSvgPath }}
+              </v-icon>
+              <a href="https://memo.portability.info" class="text-decoration-none secondary--text text--lighten-2">memo.portability.info</a>
+            </div>
+            <!-- <div class="d-flex mb-2">
+              <v-icon color="secondary lighten-1" class="mr-2">
+                {{ mdiPostSvgPath }}
+              </v-icon>
+              <a href="https://en.blog.tasbit.net" class="text-decoration-none secondary--text text--lighten-2">en.blog.tasbit.net</a>
+            </div> -->
+            <!-- <div class="d-flex mb-2">
+              <v-icon color="secondary lighten-1" class="mr-2">
+                {{ mdiPostSvgPath }}
+              </v-icon>
+              <a href="https://ja.blog.tasbit.net" class="text-decoration-none secondary--text text--lighten-2">ja.blog.tasbit.net</a>
+            </div> -->
+          </v-col>
+          <v-col cols="12" md="4">
+            <div class="text-h6 font-weight-bold text-uppercase mb-5">
+              Terms and Privacy
+            </div>
+            <div class="d-flex mb-2">
+              <v-icon color="secondary lighten-1" class="mr-2">
+                {{ mdiPostSvgPath }}
+              </v-icon>
+              <a href="https://ja.blog.tasbit.net/terms/" class="text-decoration-none secondary--text text--lighten-2">Terms</a>
+            </div>
+            <div class="d-flex mb-2">
+              <v-icon color="secondary lighten-1" class="mr-2">
+                {{ mdiPostSvgPath }}
+              </v-icon>
+              <a href="https://ja.blog.tasbit.net/privacy/" class="text-decoration-none secondary--text text--lighten-2">Privacy</a>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-divider class="my-4" />
+            <a href="http://www.freepik.com">Top image designed by vectorjuice / Freepik</a>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+import { mdiPost, mdiTwitter, mdiGithub } from '@mdi/js'
+export default {
+  data () {
+    return {
+      mdiPostSvgPath: mdiPost,
+      mdiTwitterSvgPath: mdiTwitter,
+      mdiGithubSvgPath: mdiGithub,
+      links: [{
+        label: 'Overview',
+        to: '#'
+      }, {
+        label: 'Features',
+        to: '#'
+      }, {
+        label: 'Pricing',
+        to: '#'
+      }, {
+        label: 'Documentation',
+        to: '#'
+      }, {
+        label: 'News',
+        to: '#'
+      }, {
+        label: 'FAQ',
+        to: '#'
+      }, {
+        label: 'About us',
+        to: '#'
+      }, {
+        label: 'Carrers',
+        to: '#'
+      }, {
+        label: 'Press',
+        to: '#'
+      }]
+    }
+  }
+}
+</script>
---
+++ components/component/LandingPage/LandingPageHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <v-app-bar
+      color="white"
+      fixed
+    >
+      <!-- <v-app-bar-nav-icon /> -->
+      <img src="/v-logo-small.png" alt="" height="30" class="mr-1">
+      <v-toolbar-title>
+        Remenu
+      </v-toolbar-title>
+
+      <v-spacer />
+
+      <!-- <div class="mx-1">
+        <v-btn color="primary" @click="clickLang">
+          {{ lang }}
+        </v-btn>
+      </div> -->
+      <div class="mx-1">
+        <v-btn color="primary" @click="toSignUp">
+          登録
+        </v-btn>
+      </div>
+      <div class="mx-1">
+        <v-btn color="primary" @click="toSignIn">
+          ログイン
+        </v-btn>
+      </div>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    lang () {
+      if (this.$i18n.locale === 'en') {
+        return '日本語'
+      } else {
+        return 'English'
+      }
+      // return this.$i18n.lang
+    }
+  },
+  methods: {
+    toSignUp () {
+      this.$router.push(this.localeRoute({ name: 'signup' }))
+    },
+    toSignIn () {
+      this.$router.push(this.localeRoute({ name: 'signin' }))
+    },
+    clickLang () {
+      if (this.$i18n.locale === 'en') {
+        this.$i18n.locale = 'ja'
+        // this.$router.push({ path: '/ja' })
+        // this.$router.push({ path: '/' })
+      } else {
+        this.$i18n.locale = 'en'
+        // this.$router.push({ path: '/en' })
+      }
+    }
+  }
+}
+</script>

697f25f63c08d6f50cab3da2c515a66c8cce1d55
make-ship <50040766+make-ship@users.noreply.github.com>
2021-05-01 23:06:51 +0900

    Update index_x.vue

--- pages/index_x.vue
+++ pages/index_x.vue
@@ -2,8 +2,6 @@
   <v-row justify="center" align="center">
     <v-col cols="11" sm="10" md="9">
       <div class="text-center">
-        <!-- <logo /> -->
-        <!-- <vuetify-logo /> -->
         <Hero />
         <TopView
           class="mt-6"
@@ -16,8 +14,6 @@
 </template>
 
 <script>
-// import Logo from '~/components/Logo.vue'
-// import VuetifyLogo from '~/components/VuetifyLogo.vue'
 import Hero from '~/components/component/landingPage/Hero.vue'
 import Feature from '~/components/component/landingPage/Feature.vue'
 import Pricing from '~/components/component/landingPage/Pricing.vue'
@@ -25,8 +21,6 @@ import TopView from '~/components/component/TopView.vue'
 
 export default {
   components: {
-    // Logo,
-    // VuetifyLogo,
     TopView,
     Hero,
     Feature,
